be277b74ba3053d79b1648d67298d4a60d348d80
yaraslau <yaraslau.maksimchyk@ceiia.com>
2025-05-04 01:04:10 +0100

    Force dark theme for testing

--- src/providers/theme-provider.tsx
+++ src/providers/theme-provider.tsx
@@ -7,6 +7,7 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
     <NextThemeProvider
       attribute='class'
       defaultTheme='dark'
+      forcedTheme='dark'
       //enableSystem
       disableTransitionOnChange
     >
