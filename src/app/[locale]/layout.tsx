08eb4f6b42fe70377a8f5ef79c8563fb0e25f319
Yaraslau Maksimchyk <90287515+iaruso@users.noreply.github.com>
2025-06-02 01:39:42 +0100

    beta-v2-repo

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -18,7 +18,7 @@ const kumbhSans = Kumbh_Sans({
 })
 
 export const metadata: Metadata = {
-  title: 'Oil Spill Monitor - 0.9.2',
+  title: 'Oil Spill Monitor - beta-v2',
   description: 'A web application to visualize oil spills on a globe',
 }
 
